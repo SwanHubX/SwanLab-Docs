e40a82ea199da79b989a8bbc1e16d99aa3663cef
ShaohonChen <shaohon_chen@115lab.club>
2024-12-08 01:43:12 +0800

    change title name

--- .vitepress/zh.ts
+++ .vitepress/zh.ts
@@ -129,7 +129,7 @@ function sidebarGuideCloud(): DefaultTheme.SidebarItem[] {
     text: '⚡️ 集成',
     // collapsed: false,
     items: [
-      { text: 'Ascend & MindSpore', link: 'integration/integration-ascend.md' },
+      { text: 'Ascend NPU & MindSpore', link: 'integration/integration-ascend.md' },
       { text: 'Argparse', link:'integration/integration-argparse' },
       { text: 'Fastai', link: 'integration/integration-fastai' },
       { text: 'HuggingFace Accelerate', link: 'integration/integration-huggingface-accelerate' },
