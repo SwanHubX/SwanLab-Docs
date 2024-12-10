df5ea070fdb634a977deb0f5f113434a4f03dfec
Shaohon Chen <35194781+ShaohonChen@users.noreply.github.com>
2024-12-10 15:33:40 +0800

    docs: add openmind examples (#73)
    * add openmind support
    
    * add openmind example (#72)
    
    ---------
    
    Co-authored-by: Tina <132882328+828Tina@users.noreply.github.com>

--- .vitepress/zh.ts
+++ .vitepress/zh.ts
@@ -198,6 +198,7 @@ function sidebarExamples(): DefaultTheme.SidebarItem[] {
       { text: 'BERT文本分类', link: 'bert' },  
       { text: 'Qwen微调案例', link: 'qwen_finetune' },  
       { text: 'LLM预训练', link: 'pretrain_llm' },  
+      { text: 'openMind大模型微调', link: 'openMind' },  
     ]
   },
   {
