95a404ecc4e08bf7da58141c9e79f926b111f4fc
ZeYi Lin <944270057@qq.com>
2025-06-02 16:28:26 +0800

    py-echarts

--- .vitepress/en.ts
+++ .vitepress/en.ts
@@ -330,6 +330,7 @@ function sidebarAPI(): DefaultTheme.SidebarItem[] {
         { text: 'Image', link: 'py-Image' },
         { text: 'Audio', link: 'py-Audio' },
         { text: 'Text', link: 'py-Text' },
+        { text: 'Echarts', link: 'py-echarts' },
         { text: 'Object3D', link: 'py-object3d' },
         { text: 'Molecule', link: 'py-molecule' },
       ]},
