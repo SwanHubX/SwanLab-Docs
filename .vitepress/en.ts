911a59e127ad26fa09605c5c92028e86565db085
ZeYi Lin <944270057@qq.com>
2025-09-10 11:39:07 +0800

    version number

--- .vitepress/en.ts
+++ .vitepress/en.ts
@@ -57,7 +57,7 @@ export const en = defineConfig({
         activeMatch: '/en/api/',
         },
       { text: 'Plugin', link: base_path_plugin + '/plugin-index'},
-      { text: 'v0.6.8',  items: [
+      { text: 'v0.6.9',  items: [
         { text: 'Changelog', link: base_path_guide_cloud + '/general/changelog' },
         { text: 'Community', link: 'https://swanlab.cn/benchmarks' },
         { text: 'Join Us', link: 'https://rcnpx636fedp.feishu.cn/wiki/BxtVwAc0siV0xrkCbPTcldBEnNP' },
