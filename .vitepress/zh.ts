4ac86d9b52d3749d4fae02e93a9d5239597363f4
ShaohonChen <shaohon_chen@115lab.club>
2025-09-18 16:36:23 +0800

    add apple mlx tutorial

--- .vitepress/zh.ts
+++ .vitepress/zh.ts
@@ -346,6 +346,7 @@ function sidebarExamples(): DefaultTheme.SidebarItem[] {
       { text: 'Qwen下游任务训练', link: 'qwen_finetune' },
       { text: 'NER命名实体识别', link: 'ner' },
       { text: 'Qwen3医学模型微调', link: 'qwen3-medical' },
+      { text: '通过微调给Qwen3起新名字', link: 'mlx_lm_finetune' },
     ]
   },
   {
