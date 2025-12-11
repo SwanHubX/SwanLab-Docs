c4ffb0ef0b6e32586b584212ea59aaaa6a1f63e3
ZeYi Lin <944270057@qq.com>
2025-12-11 16:12:05 +0800

    update nvidia nemo rl name

--- .vitepress/zh.ts
+++ .vitepress/zh.ts
@@ -47,7 +47,6 @@ export const zh = defineConfig({
           { text: 'veRL', link: base_path_guide_cloud + '/integration/integration-verl' },
           { text: 'Ultralytics', link: base_path_guide_cloud + '/integration/integration-ultralytics' },
           { text: 'Sb3', link: base_path_guide_cloud + '/integration/integration-sb3' },
-          { text: 'NVIDIA-NeMo/RL', link: base_path_guide_cloud + '/integration/integration-nvidia-nemo-rl'},
         ]
       },
       {
@@ -282,7 +281,7 @@ function sidebarIntegration(): DefaultTheme.SidebarItem[] {
         { text: 'MMDetection', link: 'integration-mmdetection' },
         { text: 'MMSegmentation', link: 'integration-mmsegmentation' },
         { text: 'Modelscope Swift', link: 'integration-swift' },
-        { text: 'NVIDIA-NeMo/RL', link: 'integration-nvidia-nemo-rl' },
+        { text: 'NVIDIA-NeMo RL', link: 'integration-nvidia-nemo-rl' },
       ]
     },
     {
