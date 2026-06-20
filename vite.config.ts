e619ef6031399c5fa036f24bf231d48b3d399a86
Nexisato <978452096@qq.com>
2026-06-20 11:10:47 +0800

    build: migrate to vp

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite-plus";
+
+export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  fmt: {},
+  lint: {
+    jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
+    rules: { "vite-plus/prefer-vite-plus-imports": "error" },
+    options: { typeAware: true, typeCheck: true },
+  },
+});
